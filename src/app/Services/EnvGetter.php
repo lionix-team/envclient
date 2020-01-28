f0b23fec68037aed8b5301ee43bf47132234ea6d
vaawebdev <vaawebdev@gmail.com>
2020-01-28 17:18:51 +0400

    Getter all and has methods now parsing the file

--- src/app/Services/EnvGetter.php
+++ src/app/Services/EnvGetter.php
@@ -2,8 +2,6 @@
 
 namespace Lionix\EnvClient\Services;
 
-use Dotenv\Dotenv;
-use Lionix\EnvClient\Services\EnvSetter;
 use Lionix\EnvClient\Interfaces\EnvGetterInterface;
 
 class EnvGetter implements EnvGetterInterface
@@ -16,14 +14,24 @@ class EnvGetter implements EnvGetterInterface
     public function all() : array
     {
         $toReturn = [];
-        foreach ($_ENV as $key => $value) {
-            $toReturn[$key] = env($key);
+        $fp = fopen(app()->environmentFilePath(), "r");
+        if ($fp) {
+            while (($line = fgets($fp)) !== false) {
+                if (preg_match("/^(\w*)"."=[^\r\n]*/m", $line)) {
+                    $key = strtok($line, "=");
+                    $toReturn[$key] = env($key);
+                }
+            }
+            fclose($fp);
         }
         return $toReturn;
     }
 
     public function has(string $key) : bool
     {
-        return array_key_exists($key, $_ENV);
+        return preg_match(
+            "/^".preg_quote($key)."=[^\r\n]*/m", 
+            file_get_contents(app()->environmentFilePath())
+        );
     }
 }
